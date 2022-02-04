05cac4963eee35f6ccfa43362ff320baac461fd8
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2022-02-04 14:21:38 +0600

    added alert

--- script.js
+++ script.js
@@ -2,8 +2,16 @@ var vasya = {
   name:"Vasilyi",
   lastName:"Petrovskyi",
   age: 25,
-  greet: function(){
-    alert(`Hello, my name is ${this.name}!`);
+  greet: function(){ //methods
+    alert(`Hello, my name is ${this.name}! I am ${this.age} years old.`)
   }
-}
-vasya.greet();
+ }
+ 
+ vasya.greet();
+ vasya.greet();
+ vasya.greet();
+
+//1. Call `greet` method multiple times.
+//2. Include age in the greeting.
+//3. Try chaging name and age.
+//4. Create method `bye`.
